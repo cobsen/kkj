e22bfc96636d3c9b6180b59ce7503c8e0f67c168
cobsen <jakob@perspective-daily.de>
2023-04-11 16:20:52 +0200

    adds datenschutz, impressum db content

--- pages/datenschutz.vue
+++ pages/datenschutz.vue
@@ -1,5 +1,29 @@
 <template>
-  <section>
-    <p>Datenschutz</p>
-  </section>
+  <section v-html="datenschutz"></section>
 </template>
+
+<script lang="ts" setup>
+import { Datenschutz } from "~/assets/types";
+
+const { findOne } = useStrapi();
+
+const response = await useAsyncData("datenschutz", () =>
+  findOne<Datenschutz>("datenschutz", {
+    populate: "PDF",
+  })
+);
+
+const datenschutz = computed(() => {
+  return response?.data.value?.data.attributes.Inhalt ?? "";
+});
+</script>
+
+<style lang="scss" scoped>
+section {
+  column-count: 3;
+  column-gap: 24px;
+  &:before {
+    display: none;
+  }
+}
+</style>
--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,5 +1,29 @@
 <template>
-  <section>
-    <p>Impressum</p>
-  </section>
+  <section v-html="impressum"></section>
 </template>
+
+<script lang="ts" setup>
+import { Impressum } from "~/assets/types";
+
+const { findOne } = useStrapi();
+
+const response = await useAsyncData("impressum", () =>
+  findOne<Impressum>("impressum", {
+    populate: "PDF",
+  })
+);
+
+const impressum = computed(() => {
+  return response?.data.value?.data.attributes.Inhalt ?? "";
+});
+</script>
+
+<style lang="scss" scoped>
+section {
+  column-count: 3;
+  column-gap: 24px;
+  &:before {
+    display: none;
+  }
+}
+</style>
