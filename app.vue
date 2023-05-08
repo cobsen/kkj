6372520e571b9c9e46813f2ba388dbcfc15b49f7
cobsen <jakob@perspective-daily.de>
2023-05-08 13:19:32 +0200

    changes bg gradient

--- app.vue
+++ app.vue
@@ -1,172 +1,49 @@
 <template>
   <div id="main">
     <Navigation />
-    <OverlayScrollbarsComponent
-      :options="{ scrollbars: { autoHide: 'move' } }"
-      ref="$scrollbar"
-    >
+    <OverlayScrollbarsComponent :options="{ scrollbars: { autoHide: 'move' } }" ref="$scrollbar">
       <NuxtPage />
     </OverlayScrollbarsComponent>
   </div>
-  <div id="bg-wrap">
+  <div id="bg-wrap" style="background:#306fdd">
     <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice">
       <defs>
-        <radialGradient
-          id="Gradient1"
-          cx="50%"
-          cy="50%"
-          fx="0.441602%"
-          fy="50%"
-          r=".5"
-        >
-          <animate
-            attributeName="fx"
-            dur="34s"
-            values="0%;30%;0%"
-            repeatCount="indefinite"
-          ></animate>
-          <stop offset="0%" stop-color="rgba(255, 0, 255, 1)"></stop>
-          <stop offset="100%" stop-color="rgba(255, 0, 255, 0)"></stop>
+        <radialGradient id="Gradient1" cx="50%" cy="50%" fx="0.441602%" fy="50%" r=".5">
+          <animate attributeName="fx" dur="34s" values="0%;30%;0%" repeatCount="indefinite"></animate>
+          <stop offset="0%" stop-color="rgba(255, 0, 255, .7)"></stop>
+          <stop offset="70%" stop-color="rgba(255, 0, 255, 0)"></stop>
         </radialGradient>
-        <radialGradient
-          id="Gradient2"
-          cx="50%"
-          cy="50%"
-          fx="2.68147%"
-          fy="50%"
-          r=".5"
-        >
-          <animate
-            attributeName="fx"
-            dur="23.5s"
-            values="0%;30%;0%"
-            repeatCount="indefinite"
-          ></animate>
-          <stop offset="0%" stop-color="rgba(255, 255, 0, 1)"></stop>
-          <stop offset="100%" stop-color="rgba(255, 255, 0, 0)"></stop>
+        <radialGradient id="Gradient2" cx="50%" cy="50%" fx="2.68147%" fy="50%" r=".5">
+          <animate attributeName="fx" dur="23.5s" values="0%;30%;0%" repeatCount="indefinite"></animate>
+          <stop offset="0%" stop-color="rgba(255, 255, 0, .8)"></stop>
+          <stop offset="70%" stop-color="rgba(255, 255, 0, 0)"></stop>
         </radialGradient>
-        <radialGradient
-          id="Gradient3"
-          cx="50%"
-          cy="50%"
-          fx="0.836536%"
-          fy="50%"
-          r=".5"
-        >
-          <animate
-            attributeName="fx"
-            dur="21.5s"
-            values="0%;30%;0%"
-            repeatCount="indefinite"
-          ></animate>
-          <stop offset="0%" stop-color="rgba(0, 255, 255, 1)"></stop>
-          <stop offset="100%" stop-color="rgba(0, 255, 255, 0)"></stop>
+        <radialGradient id="Gradient3" cx="50%" cy="50%" fx="0.836536%" fy="50%" r=".5">
+          <animate attributeName="fx" dur="21.5s" values="0%;30%;0%" repeatCount="indefinite"></animate>
+          <stop offset="0%" stop-color="rgba(0, 255, 255, 0.6)"></stop>
+          <stop offset="70%" stop-color="rgba(0, 255, 255, 0)"></stop>
         </radialGradient>
-        <radialGradient
-          id="Gradient4"
-          cx="50%"
-          cy="50%"
-          fx="4.56417%"
-          fy="50%"
-          r=".5"
-        >
-          <animate
-            attributeName="fx"
-            dur="23s"
-            values="0%;30%;0%"
-            repeatCount="indefinite"
-          ></animate>
-          <stop offset="0%" stop-color="rgba(0, 255, 0, 1)"></stop>
-          <stop offset="100%" stop-color="rgba(0, 255, 0, 0)"></stop>
-        </radialGradient>
+
       </defs>
 
-      <rect
-        x="13.744%"
-        y="1.18473%"
-        width="100%"
-        height="100%"
-        fill="url(#Gradient1)"
-        transform="rotate(334.41 50 50)"
-      >
-        <animate
-          attributeName="x"
-          dur="40s"
-          values="25%;0%;25%"
-          repeatCount="indefinite"
-        ></animate>
-        <animate
-          attributeName="y"
-          dur="21s"
-          values="0%;10%;0%"
-          repeatCount="indefinite"
-        ></animate>
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          from="0 50 50"
-          to="360 50 50"
-          dur="20s"
-          repeatCount="indefinite"
-        ></animateTransform>
+      <rect x="13.744%" y="1.18473%" width="100%" height="100%" fill="url(#Gradient1)" transform="rotate(334.41 50 50)">
+        <animate attributeName="x" dur="40s" values="25%;0%;25%" repeatCount="indefinite"></animate>
+        <animate attributeName="y" dur="21s" values="0%;10%;0%" repeatCount="indefinite"></animate>
+        <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="12s"
+          repeatCount="indefinite"></animateTransform>
       </rect>
-      <rect
-        x="-2.17916%"
-        y="35.4267%"
-        width="100%"
-        height="100%"
-        fill="url(#Gradient2)"
-        transform="rotate(255.072 50 50)"
-      >
-        <animate
-          attributeName="x"
-          dur="40s"
-          values="-25%;0%;-25%"
-          repeatCount="indefinite"
-        ></animate>
-        <animate
-          attributeName="y"
-          dur="24s"
-          values="0%;10%;0%"
-          repeatCount="indefinite"
-        ></animate>
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          from="0 50 50"
-          to="360 50 50"
-          dur="20s"
-          repeatCount="indefinite"
-        ></animateTransform>
+      <rect x="-2.17916%" y="35.4267%" width="100%" height="100%" fill="url(#Gradient2)"
+        transform="rotate(255.072 50 50)">
+        <animate attributeName="x" dur="40s" values="-25%;0%;-25%" repeatCount="indefinite"></animate>
+        <animate attributeName="y" dur="24s" values="0%;10%;0%" repeatCount="indefinite"></animate>
+        <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="12s"
+          repeatCount="indefinite"></animateTransform>
       </rect>
-      <rect
-        x="9.00483%"
-        y="14.5733%"
-        width="100%"
-        height="100%"
-        fill="url(#Gradient3)"
-        transform="rotate(139.903 50 50)"
-      >
-        <animate
-          attributeName="x"
-          dur="40s"
-          values="0%;25%;0%"
-          repeatCount="indefinite"
-        ></animate>
-        <animate
-          attributeName="y"
-          dur="12s"
-          values="0%;10%;0%"
-          repeatCount="indefinite"
-        ></animate>
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          from="360 50 50"
-          to="0 50 50"
-          dur="20s"
-          repeatCount="indefinite"
-        ></animateTransform>
+      <rect x="9.00483%" y="14.5733%" width="100%" height="100%" fill="url(#Gradient3)" transform="rotate(139.903 50 50)">
+        <animate attributeName="x" dur="40s" values="0%;25%;0%" repeatCount="indefinite"></animate>
+        <animate attributeName="y" dur="12s" values="0%;10%;0%" repeatCount="indefinite"></animate>
+        <animateTransform attributeName="transform" type="rotate" from="360 50 50" to="0 50 50" dur="12s"
+          repeatCount="indefinite"></animateTransform>
       </rect>
     </svg>
   </div>
@@ -219,6 +96,7 @@ watch(search, (newHash, _) => {
   height: 100vh;
   z-index: 1;
   position: relative;
+
   @include breakpoint(mobile) {
     padding: 24px 32px 0;
   }
@@ -232,6 +110,7 @@ watch(search, (newHash, _) => {
   right: 0;
   overflow: hidden;
   background: #fca58d;
+
   svg {
     height: 100%;
     width: 100%;
@@ -241,6 +120,7 @@ watch(search, (newHash, _) => {
 section {
   box-sizing: border-box;
   margin-bottom: 48px;
+
   &.divided:before {
     content: " ";
     display: block;
@@ -249,11 +129,13 @@ section {
     height: 3px;
     background-clip: content-box;
   }
+
   .content {
     //white-space: pre-wrap;
     padding: 0 20px 0 0;
   }
 }
+
 body {
   font-family: "Space Grotesk", sans-serif;
   font-size: 20px;
@@ -261,15 +143,18 @@ body {
   color: $main;
   -webkit-font-smoothing: antialiased;
 }
+
 .os-scrollbar {
   margin: 32px 0;
   --os-handle-bg: rgba(255, 255, 255, 0.4);
   --os-handle-bg-hover: rgba(255, 255, 255, 0.55);
   --os-handle-bg-active: rgba(255, 255, 255, 0.66);
 }
+
 .os-viewport {
   scroll-behavior: smooth !important;
 }
+
 button {
   padding: 12px 20px;
   border: 3px solid #fff;
@@ -280,9 +165,9 @@ button {
   font-weight: bold;
   cursor: pointer;
   transition: 0.2s;
+
   &:hover {
     background: rgba(#fff, 0.4); //#fff;
     color: #fff; //#fca58d;
   }
-}
-</style>
+}</style>
